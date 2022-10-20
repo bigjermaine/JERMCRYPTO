e183eebd690e1f2decee518f3cd0a4d1388d50dc
daniel jermaine <danieljermaine2@gmail.com>
2022-10-20 12:19:07 +0100

    Initial Commit

---
+++ JERMCRYPTO/JERMCRYPTOApp.swift
@@ -0,0 +1,17 @@
+//
+//  JERMCRYPTOApp.swift
+//  JERMCRYPTO
+//
+//  Created by Apple on 20/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct JERMCRYPTOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
